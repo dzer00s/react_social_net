a8c9fd15cce5485853d8c2f0b409e23b133d928c
d2e2 <62199717+d2e2@users.noreply.github.com>
2020-07-16 18:52:23 +0300

    auth_reducer & login
    added auth_reducer
    added HeaderContainer
    added login in Header

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from './Components/Header/Header';
 import NavBar from './Components/Navbar/Nav';
 import { Route, BrowserRouter } from 'react-router-dom';
-import Music from './Components/Music/Music';
 import Settings from './Components/Settings/Settings';
 import DialogsContainer from './Components/Dialogs/DialogsContainer';
 import UsersContainer from './Components/Users/UsersContainer';
 import ProfileContainer from './Components/Profile/ProfileContainer';
+import HeaderContainer from './Components/Header/HeaderContainer';
 
 const App = (props) => {
   return (
     <div className='app-wrapper'>
-      <Header />
+      <HeaderContainer />
       <NavBar state={props.state.SideBar} />
       <div className='app-wrapper-content'>
 
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import s from './Header.module.css';
+import { NavLink } from 'react-router-dom';
 
 
-const Header = () => {
+const Header = (props) => {
   return (
     <header className={s.header}>
       <div className={s.img_icon}>
         <img src="https://ekb.toshiba-official-servis.ru/assets/templates/img/arrow.png"></img>
+        <div className={s.login}>
+        {props.isAuth ? props.login : <NavLink to={'/login'} >Login</NavLink>}
+        </div>
       </div>
     </header>
   )
